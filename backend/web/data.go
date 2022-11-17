0591086b2215cae954f03e8e5814e639bcb0ea14
Zvonimir Pervan <z.pervan65@gmail.com>
2022-11-17 14:29:16 +0100

    Update visuals and add some dummy pages

--- backend/web/data.go
+++ backend/web/data.go
@@ -16,4 +16,5 @@ type Comment struct {
 	Reply        string
 }
 
-var postMock = Post{Title: "This is a test title", Body: "Some beautifully written text with meaningful content.", Date: "01/01/1970", Author: "John Doe"}
+var postMock = Post{Title: "This is a test title", Body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultricies, tortor consectetur rhoncus sodales, arcu eros sollicitudin est, ac viverra lorem lectus at ligula. Nulla tempus tortor purus. In eget est bibendum, accumsan urna ultrices, porta eros. Cras ut congue lorem, non pulvinar dolor. Quisque sit amet sapien malesuada, aliquam purus quis, aliquam ligula. Etiam tristique, tortor vel tincidunt lacinia, est tortor pellentesque neque, ac pretium nisl sapien quis elit. Vestibulum eleifend velit fringilla pellentesque euismod. Duis fringilla dapibus velit, id mattis leo facilisis sit amet. Sed interdum euismod finibus.", Date: "01/01/1970", Author: "John Doe"}
+var postMocks = []Post{postMock, {Title: "This is a new test title", Body: "Some ugly written text with cringe content.", Date: "01/01/1969", Author: "Booby Hobby"}}
