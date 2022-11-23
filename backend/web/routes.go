f50f4ac2d13bfa27e79b23e33d9cd4c874c52330
Zvonimir Pervan <z.pervan65@gmail.com>
2022-11-23 10:08:36 +0100

    Add post fetching based on ID

--- backend/web/routes.go
+++ backend/web/routes.go
@@ -21,6 +21,7 @@ func (r Routes)HandleRequests() *mux.Router {
 
 	// GET
 	router.Path("/posts").Methods("GET").HandlerFunc(r.Handlers.getPosts)
+    router.Path("/posts/{id}").Methods("GET").HandlerFunc(r.Handlers.getPostById)
 
 	// POST
     router.Path("/posts").Methods("POST").HandlerFunc(r.Handlers.createPost)
