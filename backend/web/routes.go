a9d50d96b8d32910e551a4da91503656bbc8e337
Zvonimir Pervan <z.pervan65@gmail.com>
2022-11-25 09:09:24 +0100

    Add fetch user handler and route

--- backend/web/routes.go
+++ backend/web/routes.go
@@ -23,6 +23,7 @@ func (r Routes)HandleRequests() *mux.Router {
 	// GET
     router.Path("/posts").Methods(http.MethodGet).HandlerFunc(r.Handlers.getPosts)
     router.Path("/posts/{id}").Methods(http.MethodGet).HandlerFunc(r.Handlers.getPostById)
+    router.Path("/users/{username}").Methods(http.MethodGet).HandlerFunc(r.Handlers.getUserByUsername)
 
 	// POST
     router.Path("/posts").Methods(http.MethodPost).HandlerFunc(r.Handlers.createPost)
