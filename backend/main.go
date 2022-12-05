88f1e79576ca86023b2017ef2a59143b7308c82c
Zvonimir Pervan <z.pervan65@gmail.com>
2022-12-05 11:24:44 +0100

    Implement session manager and adjust database

--- backend/main.go
+++ backend/main.go
@@ -5,15 +5,27 @@ import (
 	"digitalpaper/backend/core/logger"
 	"digitalpaper/backend/web"
 	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/alexedwards/scs/mongodbstore"
 )
 
 func main() {
+	logger := logger.New()
+	logger.Info("Initializing dependencies")
+
+	// Core application functionalities initialization
 	app := &core.Application{}
-	app.Log = logger.New()
-
-    app.Log.Info("Initializing dependencies")
+	app.Log = logger
+	
+	app.SessionManager = scs.New()
+	app.SessionManager.Lifetime = 12 * time.Hour
+	
     router := web.NewRoutes(app)
+	app.SessionManager.Store = mongodbstore.New(router.Handlers.Database.Sessions.Database())
 
+	// Starting server functionalities
 	app.Log.Info("Starting API server")
 	err := http.ListenAndServe(":3500", router.HandleRequests())
 
--- backend/web/routes.go
+++ backend/web/routes.go
@@ -1,45 +1,48 @@
 package web
 
 import (
-    "digitalpaper/backend/core"
-    "github.com/gorilla/mux"
-    "net/http"
+	"digitalpaper/backend/core"
+	"github.com/gorilla/mux"
+	"github.com/justinas/alice"
+	"net/http"
 )
 
 type Routes struct {
-    App *core.Application
-    Handlers *Handler
+	App      *core.Application
+	Handlers *Handler
 }
 
-func NewRoutes(app *core.Application) *Routes{
-    routes := &Routes{}
-    routes.App = app
-    routes.Handlers = NewHandler(routes.App)
+func NewRoutes(app *core.Application) *Routes {
+	routes := &Routes{}
+	routes.App = app
+	routes.Handlers = NewHandler(routes.App)
 
-    return routes
+	return routes
 }
 
-
-func (r Routes)HandleRequests() *mux.Router {
+func (r Routes) HandleRequests() *mux.Router {
 	router := mux.NewRouter()
 
+    dynamicMiddleware := alice.New(r.App.SessionManager.LoadAndSave)
+    
 	// GET
-    router.Path("/posts").Methods(http.MethodGet).HandlerFunc(r.Handlers.getPosts)
-    router.Path("/posts/{id}").Methods(http.MethodGet).HandlerFunc(r.Handlers.getPostById)
-    router.Path("/users").Methods(http.MethodGet).HandlerFunc(r.Handlers.getUsers)
-    router.Path("/users/{username}").Methods(http.MethodGet).HandlerFunc(r.Handlers.getUserByUsername)
+	router.Path("/posts").Methods(http.MethodGet).Handler(dynamicMiddleware.ThenFunc(r.Handlers.getPosts))
+	router.Path("/posts/{id}").Methods(http.MethodGet).Handler(dynamicMiddleware.ThenFunc(r.Handlers.getPostById))
+	router.Path("/users").Methods(http.MethodGet).Handler(dynamicMiddleware.ThenFunc(r.Handlers.getUsers))
+	router.Path("/users/{username}").Methods(http.MethodGet).Handler(dynamicMiddleware.ThenFunc(r.Handlers.getUserByUsername))
 
 	// POST
-    router.Path("/posts").Methods(http.MethodPost).HandlerFunc(r.Handlers.createPost)
-    router.Path("/users").Methods(http.MethodPost).HandlerFunc(r.Handlers.createUser)
+	router.Path("/posts").Methods(http.MethodPost).Handler(dynamicMiddleware.ThenFunc(r.Handlers.createPost))
+	router.Path("/users").Methods(http.MethodPost).Handler(dynamicMiddleware.ThenFunc(r.Handlers.createUser))
 
-    // PUT
-    router.Path("/posts").Methods(http.MethodPut).HandlerFunc(r.Handlers.editPost)
-    router.Path("/users").Methods(http.MethodPut).HandlerFunc(r.Handlers.editUser)
+	// PUT
+	router.Path("/posts").Methods(http.MethodPut).Handler(dynamicMiddleware.ThenFunc(r.Handlers.editPost))
+	router.Path("/users").Methods(http.MethodPut).Handler(dynamicMiddleware.ThenFunc(r.Handlers.editUser))
 
-    // DELETE
-    router.Path("/posts/{id}").Methods(http.MethodDelete).HandlerFunc(r.Handlers.deletePost)
-    router.Path("/users/{username}").Methods(http.MethodDelete).HandlerFunc(r.Handlers.deleteUser)
+	// DELETE
+	router.Path("/posts/{id}").Methods(http.MethodDelete).Handler(dynamicMiddleware.ThenFunc(r.Handlers.deletePost))
+	router.Path("/users/{username}").Methods(http.MethodDelete).Handler(dynamicMiddleware.ThenFunc(r.Handlers.deleteUser))
 
+    // @TODO: Add middleware functionalities (i.e. security headers)
 	return router
 }
