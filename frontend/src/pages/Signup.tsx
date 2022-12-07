3b17b24ea24a578990a857d7c4c0714cf77c8137
m2lovric <m2lovric@gmail.com>
2022-12-07 23:54:07 +0100

    Implement addUser function

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,38 +1,95 @@
+import { useState } from 'react';
 import Layout from '../components/Layout';
 
 const Signup = () => {
+  const [data, setData] = useState({
+    username: '',
+    name: '',
+    surname: '',
+    mail: '',
+    password: '',
+  });
+
+  const addUser = async (e: React.FormEvent, url: string) => {
+    e.preventDefault();
+    const res = await fetch(url, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    setData({
+      username: '',
+      name: '',
+      surname: '',
+      mail: '',
+      password: '',
+    });
+    return res.json();
+  };
+
   return (
     <Layout>
       <main className='flex justify-center'>
         <section className='w-1/2 flex flex-col justify-center'>
           <h2 className='text-white text-3xl mb-6 font-extrabold'>Sign Up</h2>
-          <form className='flex flex-col mt-4 text-main-white'>
+          <form
+            className='flex flex-col mt-4 text-main-white'
+            onSubmit={(e) =>
+              addUser(e, 'http://localhost:3500/users').then((res) =>
+                console.log(res)
+              )
+            }
+          >
             <section className='flex justify-between '>
               <input
                 type='text'
                 className='bg-zinc-800 rounded-md px-4 py-4 my-3 mr-1 h-12 w-1/2'
                 placeholder='First name'
+                onChange={(e) => {
+                  setData({ ...data, name: e.target.value });
+                }}
+                value={data.name}
               />
               <input
                 type='text'
                 className='bg-zinc-800 rounded-md px-4 py-4 my-3 ml-1 h-12 w-1/2'
                 placeholder='Last name'
+                onChange={(e) => {
+                  setData({ ...data, surname: e.target.value });
+                }}
+                value={data.surname}
               />
             </section>
             <input
               type='text'
               className='bg-zinc-800 rounded-md px-4 py-4 my-3 h-12'
               placeholder='Username'
+              onChange={(e) => {
+                setData({ ...data, username: e.target.value });
+              }}
+              value={data.username}
             />
             <input
               type='email'
               placeholder='Email'
               className='bg-zinc-800 rounded-md px-4 py-4 my-3 h-12'
+              onChange={(e) => {
+                setData({ ...data, mail: e.target.value });
+              }}
+              value={data.mail}
             />
             <input
               type='password'
               placeholder='Password'
               className='bg-zinc-800 rounded-md px-4 py-4 my-3 h-12'
+              onChange={(e) => {
+                setData({ ...data, password: e.target.value });
+              }}
+              value={data.password}
             />
             <button
               type='submit'
