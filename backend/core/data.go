e862e398b98505e9bdf0e5c81e511576d2b48628
Zvonimir Pervan <z.pervan65@gmail.com>
2022-12-09 16:17:05 +0100

    Add emptiness check function for User struct

--- backend/core/data.go
+++ backend/core/data.go
@@ -24,5 +24,22 @@ type Comment struct {
 	Reply        string
 }
 
+type UserLogin struct {
+	Mail     string `json:"mail" bson:"mail"`
+	Password string `json:"password" bson:"password"`
+}
+
+func (u *User)IsEmpty() bool {
+	isEmpty := true
+
+	isEmpty = isEmpty && (u.Username == "")
+	isEmpty = isEmpty && (u.Name == "")
+	isEmpty = isEmpty && (u.Surname == "")
+	isEmpty = isEmpty && (u.Mail == "")
+	isEmpty = isEmpty && (u.Password == "")
+
+	return isEmpty
+}
+
 var postMock = Post{Title: "This is a test title", Body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultricies, tortor consectetur rhoncus sodales, arcu eros sollicitudin est, ac viverra lorem lectus at ligula. Nulla tempus tortor purus. In eget est bibendum, accumsan urna ultrices, porta eros. Cras ut congue lorem, non pulvinar dolor. Quisque sit amet sapien malesuada, aliquam purus quis, aliquam ligula. Etiam tristique, tortor vel tincidunt lacinia, est tortor pellentesque neque, ac pretium nisl sapien quis elit. Vestibulum eleifend velit fringilla pellentesque euismod. Duis fringilla dapibus velit, id mattis leo facilisis sit amet. Sed interdum euismod finibus.", Date: "01/01/1970", Author: "John Doe"}
 var postMocks = []Post{postMock, {Title: "This is a new test title", Body: "Some ugly written text with cringe content.", Date: "01/01/1969", Author: "Booby Hobby"}}
